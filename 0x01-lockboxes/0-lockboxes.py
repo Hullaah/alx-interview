7bb1ace3eb58413269c09320903b9d13d2e741bf
Hullaah <umaradelo1.247@gmail.com>
2024-10-28 08:24:18 +0100

    Implemented the task using breadth first search

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,20 +8,46 @@ Determine if all the boxes can be opened.
 """
 
 
+class QueueFrontier():
+    def __init__(self) -> None:
+        self.queue = []
+
+    def add(self, key):
+        self.queue.append(key)
+
+    def remove(self):
+        key = self.queue[0]
+        self.queue = self.queue[1:]
+        return key
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def contains(self, key):
+        return key in self.queue
+
+
+def unlock(boxes, key):
+    return [box for box in boxes[key] if box >= 0 and box < len(boxes)]
+
+
 def canUnlockAll(boxes):
     """Determines if all boxes can be opened
     """
-    unclockedBoxes = set([0])
+    unclockedBoxes = set()
+    frontier = QueueFrontier()
+    frontier.add(0)
 
-    def canUnlockAllBoxesFrom(box):
+    while True:
+        if frontier.empty():
+            return False
+
+        box = frontier.remove()
+        unclockedBoxes.add(box)
+
         if len(unclockedBoxes) == len(boxes):
             return True
-        elif len(box) == 0:
-            return False
-        else:
-            if box[0] not in unclockedBoxes:
-                unclockedBoxes.add(box[0])
-                return (canUnlockAllBoxesFrom(boxes[box[0]]) or
-                        canUnlockAllBoxesFrom(box[1:]))
-            return canUnlockAllBoxesFrom(box[1:])
-    return canUnlockAllBoxesFrom(boxes[0])
+
+        for b in unlock(boxes, box):
+            if not (frontier.contains(b) or b in unclockedBoxes):
+                frontier.add(b)
