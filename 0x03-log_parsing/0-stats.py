405ab2b1a33556e887f52f67652054962ccc2255
Hullaah <umaradelo1.247@gmail.com>
2024-11-01 22:41:58 +0100

    Implemented the log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,46 +4,62 @@
 import sys
 import collections
 import re
+
 STATUS_CODES = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
 
 def print_metric(metric):
+    """prints the metric of stats gottent from the log file
+    """
     for status_code in STATUS_CODES:
-        count = metric.get(status_code)
-        if count is not None:
+        frequency = metric.get(status_code)
+        if frequency is not None:
             print("{}: {}".format(status_code, metric[status_code]))
 
 
 def main():
+    """Main driver of the program
+    """
     while True:
-        count = 0
+        num_lines = 0
         metric = collections.Counter()
         total_size = 0
 
         try:
             for line in sys.stdin:
-                if count == 10:
+                if num_lines == 10:
                     print("File size: {}".format(total_size))
                     print_metric(metric)
                     metric = collections.Counter()
                     total_size = 0
-                (status_code, file_size) = parse(line)
-                total_size += file_size
-                metric.update([status_code])
-                count += 1
+                    num_lines = 0
+                (status_code, file_size) = parse(line.rstrip())
+                if status_code is not None:
+                    total_size += file_size
+                    metric.update([status_code])
+                num_lines += 1
             break
         except KeyboardInterrupt:
             print("File size: {}".format(total_size))
             print_metric(metric)
-            metric = collections.Counter()
-            total_size = 0
-            continue
 
 
-def parse(line: str) -> tuple[str, int]:
-    ip = r"\.".join([r"\d{1,2}[0-5]?"] * 4)
-    date = r""
-    ...
+def parse(line):
+    """Parses each line of the log file
+    """
+    ip = r"[.]".join([r"(1?\d{1,2}|2[0-4]\d|25[0-5])"] * 4)
+    date = r"\d{4}-(0\d|1[0-2])-([0-2]\d|3[01])"
+    time = r"([0-1]\d|2[0-3]):(0\d|[1-5]\d):(0\d|[1-5]\d)[.]\d{6}"
+    datetime = rf"\[{date} {time}\]"
+    request = r'"GET /projects/260 HTTP/1.1"'
+    status_code = r"(?P<method>" + r"|".join(STATUS_CODES) + r")"
+    filesize = r"(?P<filesize>\d+)"
+    pattern = " ".join([ip, "-", datetime, request, status_code, filesize])
+    match = re.match(rf"^{pattern}$", line)
+    try:
+        return match.group("method"), int(match.group("filesize"))
+    except (AttributeError, IndexError):
+        return None, None
 
 
 if __name__ == "__main__":
